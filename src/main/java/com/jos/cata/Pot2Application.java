6c00f8b1d3d4cbd0c573878e3952c7cbd96dd26c
tblasCode <tomas.deblas@vass.es>
2020-05-06 01:25:05 +0200

    init project

---
+++ src/main/java/com/jos/cata/Pot2Application.java
@@ -0,0 +1,13 @@
+package com.jos.cata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jos/cata/Pot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jos.cata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
